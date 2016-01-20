64b8176b8856a4492f29fa5fc1eb1d477646a161
Nicolas Reed <Nicolas.Reed.102@nd.edu>
2016-01-20 12:33:15 -0500

    Update PROJECT_ROOT to reflect updated project stucture.

--- website/settings.py
+++ website/settings.py
@@ -14,7 +14,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
-PROJECT_ROOT = os.path.join(PROJECT_PATH, os.pardir)
+PROJECT_ROOT = os.path.join(PROJECT_PATH, "apps")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
