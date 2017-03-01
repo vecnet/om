5e336c549bcd2f5ee75e51dc67ebba83632d9f72
Alexander Vyushkov <baguage@gmail.com>
2017-03-01 10:57:32 -0500

    simplifying settings

--- website/settings/qa.py
+++ website/settings/qa.py
@@ -10,7 +10,7 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 
 DATABASES = {
     'default': {
-        'ENGINE': "django.db.backends.postgresql",
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
         'NAME': get_env_variable("DATABASE_NAME"),
         'USER': get_env_variable("DATABASE_USER"),
         'PASSWORD': get_env_variable("DATABASE_PASSWORD"),
