491c1e6c2909e00dabc9b61a06779cb985cf5416
Alexander Vyushkov <baguage@gmail.com>
2017-03-02 21:05:30 -0500

    Migration clean up

--- website/apps/ts_om/migrations/0001_initial.py
+++ website/apps/ts_om/migrations/0001_initial.py
@@ -8,7 +8,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('data_services', '__first__'),
+        ('data_services', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
--- website/apps/ts_om_viz/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Document(models.Model):
-    file = models.FileField(upload_to='SurveyResults/')
