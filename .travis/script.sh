878a4e461aef4894fce3ea3b2154ced6014843dd
Alexander Vyushkov <baguage@gmail.com>
2017-03-12 15:48:21 -0400

    added -v 2 option to Travis manage.py command to see what tests are being executed

--- .travis/script.sh
+++ .travis/script.sh
@@ -2,16 +2,16 @@
 
 if [[ $TRAVIS_OS_NAME == "osx" ]]; then
 	python manage.py migrate
-	python manage.py test --liveserver=localhost:8082
+	python manage.py test --liveserver=localhost:8082 -v 2
 else
   if [[ $USE_DOCKER == "yes" ]]; then
 	  docker-compose up -d
 	  docker-compose run web /bin/sh -c "exec python manage.py migrate"
-	  docker-compose run web /bin/sh -c "exec python manage.py test --liveserver=localhost:8082"
+	  docker-compose run web /bin/sh -c "exec python manage.py test --liveserver=localhost:8082" -v 2
 	  docker-compose down
   else
 	  ./binaries/om/openMalaria -v
-	  python manage.py migrate
-	  python manage.py test --liveserver=localhost:8082
+#	  python manage.py migrate
+	  python manage.py test -v 2
   fi
 fi
