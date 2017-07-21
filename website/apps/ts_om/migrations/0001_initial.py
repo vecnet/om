8147e74b29650688255108b37c3c24a12868443f
Alexander Vyushkov <baguage@gmail.com>
2017-07-21 19:29:02 -0400

    Removed outdated models

--- website/apps/ts_om/migrations/0001_initial.py
+++ website/apps/ts_om/migrations/0001_initial.py
@@ -109,8 +109,8 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('sweep_name', models.CharField(max_length=127)),
                 ('arm_name', models.CharField(max_length=127)),
-                ('experiment', models.ForeignKey(to='ts_om.Experiment')),
-                ('scenario', models.ForeignKey(to='ts_om.Scenario')),
+                ('experiment', models.ForeignKey(to='ts_om.Experiment', on_delete=django.db.models.deletion.PROTECT)),
+                ('scenario', models.ForeignKey(to='ts_om.Scenario', on_delete=django.db.models.deletion.PROTECT)),
             ],
         ),
     ]
