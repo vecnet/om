3534b4b888c82eaa5d8edfa05877398a833b3de5
Alexander Vyushkov <baguage@gmail.com>
2017-08-05 23:57:31 -0400

    correction

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,6 +2,8 @@
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 	echo "OK"
+	pip install coverage
+	pip install coveralls
 elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
 	pip install virtualenv
 	virtualenv -p $PYTHON /tmp/venv
