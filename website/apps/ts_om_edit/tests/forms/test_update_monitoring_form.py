2f1c30ff6e5fbd31950dd7ea65b258e66ba18541
Alexander Vyushkov <baguage@gmail.com>
2017-09-09 14:09:22 -0400

    More tests

---
+++ website/apps/ts_om_edit/tests/forms/test_update_monitoring_form.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the VecNet OpenMalaria Portal.
+# For copyright and licensing information about this package, see the
+# NOTICE.txt and LICENSE.txt files in its top-level directory; they are
+# available at https://github.com/vecnet/om
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License (MPL), version 2.0.  If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# -*- coding: utf-8 -*-
+#
+# This file is part of the VecNet OpenMalaria Portal.
+# For copyright and licensing information about this package, see the
+# NOTICE.txt and LICENSE.txt files in its top-level directory; they are
+# available at https://github.com/vecnet/om
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License (MPL), version 2.0.  If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import json
+
+from django.test import TestCase
+from django.test.client import Client
+from django.urls import reverse
+
+from website.apps.ts_om.tests.factories import ScenarioFactory, UserFactory
+from website.apps.ts_om_edit.tests.views.test_ScenarioSummaryView import get_xml
+
+
+class UpdateEntomologyFormTest(TestCase):
+    def setUp(self):
+        self.scenario = ScenarioFactory()
+        self.client.login(username=self.scenario.user.username, password="1")
+        self.url = reverse("ts_om.monitoring.update.form", kwargs={"scenario_id": self.scenario.id})
+        self.data = {
+            "xml": get_xml("group 8 baseline p2.xml"),
+            "start_date": 1,
+        }
+
+    def test_anonymous(self):
+        client = Client()
+        response = client.post(self.url, data=self.data)
+        self.assertEqual(response.status_code, 403)
+
+    def test_invalid_xml(self):
+        response = self.client.post(self.url, data={"xml": ""})
+        self.assertEqual(response.status_code, 200)
+        json_content = json.loads(response.content)
+        self.assertEqual(json_content["result"], -1)
+
+    def test_scenario_doesnt_exist(self):
+        url = reverse("ts_om.monitoring.update.form", kwargs={"scenario_id": 1000000})
+        response = self.client.post(url, data=self.data)
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_as_different_user(self):
+        client = Client()
+        user = UserFactory()
+        client.login(username=user.username, password="1")
+        response = client.post(self.url, data=self.data)
+        self.assertEqual(response.status_code, 403)
+
+    def test(self):
+        response = self.client.post(self.url, data=self.data)
+        self.assertEqual(response.status_code, 200)
+        # self.assertEqual(response.content, "{'valid': true}")
--- website/apps/ts_om_edit/utils.py
+++ website/apps/ts_om_edit/utils.py
@@ -10,10 +10,10 @@
 # with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import json
-from xml.etree.ElementTree import ParseError
 
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 from vecnet.openmalaria.scenario import Scenario
 
 from website.apps.ts_om.models import Scenario as ScenarioModel
@@ -21,8 +21,8 @@ from website.apps.ts_om.views.ScenarioValidationView import rest_validate
 
 
 def update_form(request, scenario_id):
-    if not request.user.is_authenticated() or not scenario_id or scenario_id < 0:
-        return
+    if not request.user.is_authenticated:
+        raise PermissionDenied
 
     xml_file = request.POST['xml']
     json_str = rest_validate(xml_file)
@@ -33,16 +33,11 @@ def update_form(request, scenario_id):
     if not valid:
         return HttpResponse(json_str, content_type="application/json")
 
-    model_scenario = ScenarioModel.objects.get(id=scenario_id)
-    if model_scenario is None:
-        return HttpResponse(json.dumps({'valid': False}), content_type="application/json")
+    model_scenario = get_object_or_404(ScenarioModel, id=scenario_id)
 
     if request.user != model_scenario.user:
         raise PermissionDenied
 
-    try:
-        temp_scenario = Scenario(xml_file)
-    except ParseError:
-        return HttpResponse(json.dumps({'valid': False}), content_type="application/json")
+    temp_scenario = Scenario(xml_file)
 
     return {"valid": valid, "scenario": temp_scenario}
