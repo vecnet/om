c692c27cdb101396056e597103da52136bc1a719
Alexander Vyushkov <baguage@gmail.com>
2017-09-09 11:48:52 -0400

    tests for management command

---
+++ website/apps/ts_om/tests/commands/test_submit_scenario.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the VecNet OpenMalaria Portal.
+# For copyright and licensing information about this package, see the
+# NOTICE.txt and LICENSE.txt files in its top-level directory; they are
+# available at https://github.com/vecnet/om
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License (MPL), version 2.0.  If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from django.core.management import call_command
+from django.test.testcases import TestCase
+
+from website.apps.ts_om.tests.factories import ScenarioFactory
+
+
+class SubmitScenarioTest(TestCase):
+    def setUp(self):
+        self.scenario = ScenarioFactory()
+
+    def test_scenario_doesnt_exist(self):
+        call_command("submit_scenario", scenario_id=1000000, wait=True)
+        # Make sure that there is no exception
+        self.assertTrue(True)
+
+    def test_success(self):
+        call_command("submit_scenario", scenario_id=self.scenario.id)
+        # Make sure that there is no exception
+        self.assertTrue(True)
