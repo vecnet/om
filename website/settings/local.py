7dbf0dbd0e5c3092871b11eed6d5cd9c8fef7575
Alex Vyushkov <baguage@gmail.com>
2018-05-05 12:07:58 -0400

    Fixed a bug - internal server error when using "monthly" monitoring

--- website/settings/local.py
+++ website/settings/local.py
@@ -1,11 +1,11 @@
 from .base import *
 
 
-SECRET_KEY = get_env_variable("SECRET_KEY")
+SECRET_KEY = "lskfjaldfisdofjadfidsfsdf"
 
 DATABASES = {
     'default': {
-        'ENGINE': get_env_variable("DATABASE_ENGINE"),
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
         'NAME': get_env_variable("DATABASE_NAME"),
         'USER': get_env_variable("DATABASE_USER"),
         'PASSWORD': get_env_variable("DATABASE_PASSWORD"),
